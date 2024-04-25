d41b599ca54631edfe9a9a0ea99640863fb1e098
Colin Knox <colinknox@66405s-iMac.rule.lan>
2024-04-25 12:21:58 -0400

    Added matrix and 2 inputs.

--- GitPractice.py
+++ GitPractice.py
@@ -1,3 +1,13 @@
-greeting = "Hello, World!"
+user = input("How's it going? Tell me, what's your name? ")
+age = input("It's a pleasure to meet you " + user + "! Just curious, how old are you? ")
+final_message = print(age + "? Very cool!")
 
-print(greeting)
+matrix = [
+    [1,2,3,4,5],
+    [2,4,6,8,10],
+    [3,6,9,12,15],
+    [4,[[8,[12,[16]]],20]],
+    [5,10,15,20,25]
+]
+
+print(matrix[3][1][0][1][1][0])
